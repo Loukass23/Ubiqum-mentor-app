f4d658e4905c805ed99c3f21f20169998fd3ad20
loukass23 <l.dupias@net.estia.fr>
2019-11-18 17:54:45 +0100

    move to oAuth for demo

--- routes/students.js
+++ routes/students.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const GoogleSpreadsheet = require('google-spreadsheet');
 const creds = require('../client_secret.json');
-const  spreadSheets = require('../config/keys')
+const spreadSheets = require('../config/keys')
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    var doc = new GoogleSpreadsheet(spreadSheets.management);
+    var doc = new GoogleSpreadsheet('1XPW-VvaRbdIc3NraRkVi0_jm886pnubZI3z19ifFE-M');
 
     // Authentication
     doc.useServiceAccountAuth(creds, (err) => {
@@ -18,8 +18,8 @@ router.get("/", (req, res) => {
             }, (err, rows) => {
                 console.log(err);
                 res.send(rows.map(row => {
-                    const { program, cohort, name, email, module , sprint, day  } = row
-                    return { program, cohort, name, email , module , sprint , day }
+                    const { program, cohort, name, email, module, sprint, day } = row
+                    return { program, cohort, name, email, module, sprint, day }
 
                 }))
             })
